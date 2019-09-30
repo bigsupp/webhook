6fb1b4f315e1aa3324eea355de96dd73e3490d16
Supapong Putthayangkura <bigsupp@gmail.com>
2019-09-30 14:16:02 +0700

    add files

---
+++ server.js
@@ -0,0 +1,32 @@
+const Koa = require('koa')
+const app = new Koa()
+
+const config = require('./config')
+const router = require('koa-router')()
+const koaBody = require('koa-body')
+
+router.get('/', ctx => {
+  ctx.type = 'json'
+  ctx.body = {
+    message: 'webhook'
+  }
+})
+router.post('/webhook', ctx => {
+  const body = ctx.request.body
+  console.log('webhook received, body:', body);
+  const result = 'ok'
+  ctx.type = 'json'
+  ctx.body = {
+    body,
+    result
+  }
+})
+
+app
+  .use(koaBody())
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(config.port, () => {
+  console.log(`Server listening on ${config.port}`);
+})
